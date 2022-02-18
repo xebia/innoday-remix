56b092db71693ad749c65b495a78d24d8cf010b5
Frank van Wijk <fvanwijk@xebia.com>
2022-02-18 10:20:32 +0100

    Overview page

--- styling/app/routes/index.tsx
+++ styling/app/routes/index.tsx
@@ -1,31 +1,11 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
+      <h1>PokédeXSD</h1>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        <li>Pokemon 1</li>
+        <li>Pokemon 2</li>
+        <li>Pokemon 3</li>
       </ul>
     </div>
   );
