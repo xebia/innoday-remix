1dfbf53f19c6ba9b5a085c5137b774eb6aaf529e
Benjamin Komen <benjamin_komen@hotmail.com>
2022-02-18 10:59:19 +0100

    Styling and link back to main page

--- nextvremix/remix/app/routes/pokemons/$slug.tsx
+++ nextvremix/remix/app/routes/pokemons/$slug.tsx
@@ -1,7 +1,12 @@
 import type { LoaderFunction } from "remix";
-import { useLoaderData } from "remix";
+import { Link, useLoaderData } from "remix";
 
 import { getPokemon } from "~/pokemon";
+import pokemonStyles from "~/styles/pokemon.css";
+
+export const links = () => {
+  return [{ rel: "stylesheet", href: pokemonStyles }];
+};
 
 export const loader: LoaderFunction = async ({ params }) => {
   return getPokemon(params.slug || "");
@@ -11,8 +16,14 @@ export default function PokemonSlug() {
   const pokemon = useLoaderData();
   return (
     <div>
-      <h1>{pokemon.name}</h1>
-      <dd>Weight: {pokemon.weight}</dd>
+      <div>
+        <h1>{pokemon.name}</h1>
+        <dd>Weight: {pokemon.weight}</dd>
+      </div>
+
+      <div>
+        <Link to="/pokemons">back</Link>
+      </div>
     </div>
   );
 }
