87295d04da58b0dbe24ee5be6e8d31c20ff75895
Benjamin Komen <benjamin_komen@hotmail.com>
2022-02-18 10:01:15 +0100

    Use Remix App Server for local dev

--- nextvremix/remix/remix.config.js
+++ nextvremix/remix/remix.config.js
@@ -2,12 +2,10 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  serverBuildTarget: "netlify",
-  server: "./server.js",
   ignoredRouteFiles: [".*"]
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "netlify/functions/server/index.js",
+  // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
   // devServerPort: 8002
 };
