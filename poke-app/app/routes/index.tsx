ca672925e415c47fa4c0983d97c5e9dd47fbced9
Hernani Fernandes <hernanif1@gmail.com>
2022-02-18 14:54:22 +0100

    add some details page css

--- poke-app/app/routes/index.tsx
+++ poke-app/app/routes/index.tsx
@@ -2,11 +2,11 @@ import { Link } from "remix";
 
 export default function Index() {
   return (
-    <div className="w-full h-full bg-black flex justify-center items-center text-white">
+    <div className="w-full bg-black flex justify-center items-center text-white">
       <div className="text-center">
         <h1 className="mb-4 text-7xl font-bold">Poké App</h1>
         <Link className="poke-button" to="/pokemons">
-          start
+          Click here
         </Link>
       </div>
     </div>
--- poke-app/app/routes/pokemons/$id.tsx
+++ poke-app/app/routes/pokemons/$id.tsx
@@ -11,9 +11,27 @@ export default function Pokemon() {
   const pokemon = useLoaderData<PokemonDetails>();
 
   return (
-    <div>
-      <h1>{pokemon.name}</h1>
-      <img src={pokemon.sprites.front_default} />
+    <div className="bg-gray-200 h-full p-10 flex flex-col items-center min-w-min">
+      <div className="bg-white rounded-xl flex flex-col p-10">
+        <img src={pokemon.sprites.front_default} width="200" height="200" />
+        <h1 className="font-bold">{pokemon.name}</h1>
+        <hr />
+
+        <h3 className="font-bold mt-10">Abilities</h3>
+        <ul className="list-disc">
+          {pokemon.abilities.map((item) => (
+            <li>{item.ability.name}</li>
+          ))}
+        </ul>
+
+        <h3 className="font-bold mt-10">Forms</h3>
+        <hr />
+        <ul className="list-disc">
+          {pokemon.forms.map((item) => (
+            <li>{item.name}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
