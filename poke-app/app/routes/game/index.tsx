1e61a6471e5b0ddc58e06712bd35e10a1304450d
Hernani Fernandes <hernanif1@gmail.com>
2022-02-18 12:18:21 +0100

    fetch pokemons details

--- poke-app/app/routes/game/index.tsx
+++ poke-app/app/routes/game/index.tsx
@@ -1,7 +1,40 @@
+import { useLoaderData } from "remix";
+import { PokemonApi, PokemonDetails } from "~/types";
+
+export const loader = async () => {
+  try {
+    const pokemons: PokemonApi = await fetch(
+      "https://pokeapi.co/api/v2/pokemon?limit=100"
+    ).then((response) => response.json());
+
+    const poke: PokemonDetails[] = await Promise.all(
+      pokemons.results.map(async ({ url }) => {
+        return await fetch(url).then((response) => response.json());
+      })
+    );
+
+    return poke;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
 export default function Index() {
+  const pokemons = useLoaderData<PokemonDetails[]>();
+
   return (
-    <div className="bg-green-700">
+    <div className="bg-gray-200 h-full">
       <h1>Game</h1>
+      <div className="grid grid-cols-3 p-10">
+        {pokemons.map((pokemon) => (
+          <div key={pokemon.name} className="bg-white p-4 rounded-lg m-2 ">
+            <img src={pokemon.sprites.front_default} />
+            <div>{pokemon.name}</div>
+            <div>details</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- poke-app/app/routes/index.tsx
+++ poke-app/app/routes/index.tsx
@@ -1,17 +1,12 @@
 import { Link, MetaFunction } from "remix";
 
-export let meta: MetaFunction = () => ({
-  title: "Poke app title",
-  description: "Poke app description!",
-});
-
 export default function Index() {
   return (
     <div className="bg-red-700 w-10 h-9">
       <h1>Poké App</h1>
       <Link className="poke-button" to="/game">
         start
-      </Link>{" "}
+      </Link>
     </div>
   );
 }
