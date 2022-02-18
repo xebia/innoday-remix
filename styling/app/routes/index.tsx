822f8d57338067b8704c40f2c86a378e5001c98b
Frank van Wijk <fvanwijk@xebia.com>
2022-02-18 10:32:27 +0100

    Render pokemon list from mock

--- styling/app/routes/index.tsx
+++ styling/app/routes/index.tsx
@@ -1,11 +1,27 @@
+import { useLoaderData } from 'remix';
+import { createPokemon } from '../../mock';
+
+export type PokemonIndex = {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: { name: string; url: string }[];
+};
+
+export const loader = async () => {
+  return createPokemon();
+};
+
 export default function Index() {
+  const pokemons = useLoaderData<PokemonIndex>();
+
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <h1>PokédeXSD</h1>
       <ul>
-        <li>Pokemon 1</li>
-        <li>Pokemon 2</li>
-        <li>Pokemon 3</li>
+        {pokemons?.results.map((pokemon) => (
+          <li key={pokemon.name}>{pokemon.name}</li>
+        ))}
       </ul>
     </div>
   );
