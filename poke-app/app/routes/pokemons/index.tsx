f293c7b2e8f5ed3905814807918ceeb887783bea
Hernani Fernandes <hernanif1@gmail.com>
2022-02-18 13:38:24 +0100

    refactor api

--- poke-app/app/routes/pokemons/index.tsx
+++ poke-app/app/routes/pokemons/index.tsx
@@ -1,23 +1,10 @@
 import { Link, useLoaderData } from "remix";
+import { getAllPokemons } from "~/api/pokemons";
 import { PokemonApi, PokemonDetails } from "~/types";
 
 export const loader = async () => {
-  try {
-    const pokemons: PokemonApi = await fetch(
-      "https://pokeapi.co/api/v2/pokemon?limit=10"
-    ).then((response) => response.json());
-
-    const poke: PokemonDetails[] = await Promise.all(
-      pokemons.results.map(async ({ url }) => {
-        return await fetch(url).then((response) => response.json());
-      })
-    );
-
-    return poke;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
+  const response = await getAllPokemons();
+  return response;
 };
 
 export default function Index() {
