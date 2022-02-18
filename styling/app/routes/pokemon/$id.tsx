6263e2cd84baebf4d5f5ddcf1518576119fe63bc
Frank van Wijk <fvanwijk@xebia.com>
2022-02-18 10:48:29 +0100

    Show image from real API

--- styling/app/routes/pokemon/$id.tsx
+++ styling/app/routes/pokemon/$id.tsx
@@ -1,12 +1,15 @@
 import { createPokemon } from '../../../mock';
 import { useLoaderData } from 'remix';
 
-export const loader = async ({ params }) => {
-  return { ...createPokemon(), name: params.id };
+export const loader = async ({ params }: { params: { id: string } }) => {
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`);
 };
 
 export interface Pokemon {
   name: string;
+  sprites: {
+    front_default: string;
+  };
 }
 
 export default function Pokemon() {
@@ -15,6 +18,7 @@ export default function Pokemon() {
   return (
     <div>
       <h1>{pokemon.name}</h1>
+      <img src={pokemon.sprites.front_default} />
     </div>
   );
 }
